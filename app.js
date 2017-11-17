2b361199e3b5d11f42e893cbea90bcf1efaf08ea
Amaan Kulshreshtha <amaan.kulshreshtha@gmail.com>
2017-11-17 19:44:04 +0400

    basic skeleton

--- app.js
+++ app.js
@@ -5,4 +5,32 @@ var path = require('path');
 var hbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
+var index = require('./routes/index');
+
 var app = express();
+app.engine('hbs', hbs({ extname: "hbs", layoutsDir: __dirname + "views", defaultLayout: "layout" }));
+app.set('view engine', hbs);
+app.set('views', path.join(__dirname + "views"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname + "public")));
+
+
+app.use('/', index);
+
+app.use(function(req, res, next) {
+    var err = new Error("Error found:"); // instantiating error object
+    err.status = 404; // making the status as 404 when it is error
+    next(err); // call the next() function and pass err in the argument so that it can be used by the next function.
+});
+
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message; // when we write locals, we are telling it to render it only when we are viewing the application.
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    res.status = (err.status || 500);
+    res.render('error', { error: err });
+});
+
+module.exports = app;
